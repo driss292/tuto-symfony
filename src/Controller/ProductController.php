db8453e684a2b6b812c971ecbee21184e91fb65b
driss292 <drisskaci@outlook.fr>
2024-10-29 15:04:16 +0100

    add controller with maker-bundle

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,11 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class ProductController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/product', name: 'app_product')]
     public function index(): Response
     {
-        return $this->render("home/index.html.twig");
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
     }
-};
+}
