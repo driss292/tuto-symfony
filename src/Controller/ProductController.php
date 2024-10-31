cdfa9a101556c8455afdee34927ef4dfbeaf0821
driss292 <drisskaci@outlook.fr>
2024-10-31 15:53:19 +0100

    oh yeah

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -52,12 +52,62 @@ class ProductController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $manager->persist($product);
             $manager->flush();
+
+            $this->addFlash(
+                'notice',
+                'Product Created Successfully'
+            );
             return $this->redirectToRoute("product_show", [
                 "id" => $product->getId()
             ]);
         }
         return $this->render('product/new.html.twig', [
             'form' => $form,
+        ]);
+    }
+
+    #[Route("/product/{id<\d+>}/edit", name: "product_edit")]
+    public function edit(Product $product, Request $request, EntityManagerInterface $manager): Response
+    {
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager->flush();
+
+            $this->addFlash(
+                'notice',
+                'Product Updated Successfully'
+            );
+            return $this->redirectToRoute("product_show", [
+                "id" => $product->getId()
+            ]);
+        }
+        return $this->render('product/edit.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route("/product/{id<\d+>}/delete", name: "product_delete")]
+    public function delete(Product $product, Request $request, EntityManagerInterface $manager): Response
+    {
+
+        if ($request->isMethod('POST')) {
+            $manager->remove($product);
+
+            $manager->flush();
+            $this->addFlash(
+                'notice',
+                "'Product Deleted Successfully'"
+            );
+
+            return $this->redirectToRoute('product_index');
+        }
+        return $this->render("product/delete.html.twig", [
+            "id" => $product->getId()
         ]);
     }
 }
